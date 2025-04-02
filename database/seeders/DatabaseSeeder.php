1c75b00801aad6079318751d3d556fc2e5d9abac
PATEL HITKUMAR <hit98987@gmail.com>
2025-04-02 13:04:41 -0400

    modified databaseSeeder.php to create admin, organizer and user role initially

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,5 +20,30 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        // Create Admin user
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+        ]);
+
+        // Create Organizer user
+        User::create([
+            'name' => 'Organizer User',
+            'email' => 'organizer@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'organizer',
+        ]);
+
+        // Create Attendee user
+        User::create([
+            'name' => 'Attendee User',
+            'email' => 'attendee@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'attendee',
+        ]);
+
     }
 }
